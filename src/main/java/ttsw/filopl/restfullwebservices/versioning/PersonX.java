34c28a1a0530eca5e46faf4768707a5f50df4cf3
FiloPL <tzegarlicki@gmail.com>
2022-06-10 18:15:42 +0200

    08 - add person X and Y

---
+++ src/main/java/ttsw/filopl/restfullwebservices/versioning/PersonX.java
@@ -0,0 +1,20 @@
+package ttsw.filopl.restfullwebservices.versioning;
+
+/**
+ * Created by T. Filo Zegarlicki on 10.06.2022
+ **/
+public class PersonX {
+    private String name;
+
+    public PersonX() {
+        super();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/ttsw/filopl/restfullwebservices/versioning/PersonY.java
@@ -0,0 +1,20 @@
+package ttsw.filopl.restfullwebservices.versioning;
+
+/**
+ * Created by T. Filo Zegarlicki on 10.06.2022
+ **/
+
+public class PersonY {
+    private Name name;
+
+    public PersonY() {
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    public void setName(Name name) {
+        this.name = name;
+    }
+}
